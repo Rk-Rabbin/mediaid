cd929a4ab8b4b7b5777b8424f7abe2e4b0a3d011
Rayhan Kabir <rayhan@Rayhans-MacBook-Air.local>
2023-08-17 23:42:31 +0600

    flutter registration connected with django

--- flutter_mediaid/lib/main.dart
+++ flutter_mediaid/lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mediaid/pages/homepage.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+import 'pages/register_page.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
   runApp(const MyApp());
 }
 
@@ -15,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'API View'),
+      home: const SignUpPage(),
     );
   }
 }
